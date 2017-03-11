c932f7afc28f52642db1cfbd6e9cbf86c322d16a
Predrag Rakic <prakic@renderedtext.com>
2017-03-11 23:59:38 +0100

    send authorize request

--- lib/gh_auhenticate/router.ex
+++ lib/gh_auhenticate/router.ex
@@ -12,14 +12,24 @@ defmodule GhAuthenticate.Router do
     |> Plug.Conn.send_resp(200, page_content)
   end
 
+  get "/gh-redirect" do
+    conn
+    |> Plug.Conn.fetch_query_params(conn)
+    |> Plug.Conn.put_resp_content_type("text/html")
+    |> Plug.Conn.send_resp(200, inspect conn)
+  end
+
   match _ do send_resp(conn, 404, "oops, no page!!!") end
 
   def page_content do
   """
   <h1>Pera i mika</h1>
 
-  <form action="http://google.com">
-    <input type="submit" value="Go to Google" />
+  <form action="https://github.com/login/oauth/authorize" method="get">
+    <input type="hidden" name="client_id" value="169d6a13fb51a4b31d67" />
+    <input type="hidden" name="redirect_uri" value="https://predrag-gh-authenticate.herokuapp.com/gh-redirect" />
+    <input type="hidden" name="state" value="unique_identifier" />
+    <input type="submit" value="Authenticate with GitHub" />
   </form>
   """
   end
