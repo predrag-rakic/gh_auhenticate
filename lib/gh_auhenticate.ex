5fbbdea53274367cac0f48b73395e474aaf25442
Predrag Rakic <prakic@renderedtext.com>
2017-03-11 23:06:06 +0100

    Use PORT env var

--- lib/gh_auhenticate.ex
+++ lib/gh_auhenticate.ex
@@ -8,12 +8,16 @@ defmodule GhAuhenticate do
 
     # Define workers and child supervisors to be supervised
     children = [
-      Plug.Adapters.Cowboy.child_spec(:http, GhAuthenticate.Router, [], [port: 4000])
+      Plug.Adapters.Cowboy.child_spec(:http, GhAuthenticate.Router, [], [port: port])
     ]
 
     # See http://elixir-lang.org/docs/stable/elixir/Supervisor.html
     # for other strategies and supported options
     opts = [strategy: :one_for_one, name: GhAuhenticate.Supervisor]
     Supervisor.start_link(children, opts)
+  end
+
+  def port do
+    (System.get_env("PORT") || "4000") |> String.to_integer
   end
 end
